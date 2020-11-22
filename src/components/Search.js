78ff55d2296e62359975766cac744b2b6a552684
woodylucas <wlucas0311@gmail.com>
2020-11-21 23:54:55 -0500

    conditional rendering

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = (props) => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
           origin: "*",
           format: "json",
-          srsearc: term,
+          srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
     search();
   }, [term]);

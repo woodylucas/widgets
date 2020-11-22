b6693dcc5949f0d4c98bec82607826fdfe2c379f
woodylucas <wlucas0311@gmail.com>
2020-11-21 20:10:11 -0500

    refactored to functional components

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,34 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-class Accordion extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Accordion = ({ items }) => {
+  const handleClick = (index) => {
+    console.log("Title Clicked", index);
+  };
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div onClick={() => handleClick(index)} className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
 
-  handleClick(index) {
-    console.log("clicked", index);
-  }
-  render() {
-    const { items } = this.props;
-    const renderedItems = items.map((item, index) => {
-      return (
-        <React.Fragment key={item.title}>
-          <div
-            onClick={this.handleClick.bind(this, index)}
-            className="title active"
-          >
-            <i className="dropdown icon"></i>
-            {item.title}
-          </div>
-          <div className="content active">
-            <p>{item.content}</p>
-          </div>
-        </React.Fragment>
-      );
-    });
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
 
-    return <div className="ui styled accordion">{renderedItems}</div>;
-  }
-}
 export default Accordion;

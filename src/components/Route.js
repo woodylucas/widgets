6e8fd4f0f21afbf6097d12a5c0598b30ef4ae2a2
woodylucas <wlucas0311@gmail.com>
2020-11-30 17:42:27 -0500

    implemented command click, control click when link is used a new tab is opened to that link

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Route = ({ path, children }) => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);

5ff1413018e623d4fe28549fcb63f17f0b274151
woodylucas <wlucas0311@gmail.com>
2020-11-29 20:58:30 -0500

    basic component routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
+import Search from "./components/Search";
 import Translate from "./components/Translate";
 
 const items = [
@@ -30,10 +33,38 @@ const options = [
     value: "blue",
   },
 ];
+
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
 function App() {
   return (
     <div>
-      <Translate />
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 }
